a66d5ae2f746a4d1275588d76ce43eee336dc165
duduldy <endyd2645@naver.com>
2021-03-17 11:39:20 +0900

    네트워크 변경

--- app/src/main/java/com/duduldy/webTizenTestApp/MainActivity.java
+++ app/src/main/java/com/duduldy/webTizenTestApp/MainActivity.java
@@ -177,8 +177,8 @@ public class MainActivity extends AppCompatActivity {
 		callJSAndToWeb();
 
 		/* 웹 뷰 띄우기 */
-		//webView.loadUrl("https://naver.com/"); //접속할 URL - res/xml/network_security_config.xml에 정의 필요
-		webView.loadUrl("http://1.1.1.1:8080/"); //로컬호스트일 경우
+		webView.loadUrl("https://naver.com/"); //접속할 URL - res/xml/network_security_config.xml에 정의 필요
+		//webView.loadUrl("http://1.1.1.1:8080/"); //로컬호스트일 경우
 	}
 
 	/**
